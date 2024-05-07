9d83fa9239c36e5ce237467b8be2e3edf72d7179
Esly Caetano <eslyeslycaetano2014@gmail.com>
2024-05-07 00:09:01 +0000

    Segunda atualização do codigo da api do Star Wars, agora com react-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import './App.css'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 function App() {
@@ -38,12 +39,13 @@ function App() {
 
         <h1>{personagem && personagem.name}</h1>
 
-        {imagem && <img src={imagem} alt= "Imagem atual"/>}
+        {imagem && <img src={imagem} style={{width: "250px"}} alt= "Imagem atual"/>}
       </div>
       
       <button onClick={() => defImagem('http://www.nerdforever.com.br/image/catalog/Personagens/HanSolo-Fathead.png')} >Antes</button>
       <button onClick={() => defImagem('https://i0.wp.com/ovicio.com.br/wp-content/uploads/han-solo-harrison-ford-158748.jpg?resize=683%2C1024&ssl=1')} >Depois</button>
       
+      <Link to={'/teste'}> Link MADICIOADO</Link> 
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {path: '/', element: <App/>, errorElement: <div>ERRO 404</div>},
+  {path: '/teste', element: <div> Hello World </div>}
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router= {router}/>
   </React.StrictMode>,
 )
